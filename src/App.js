c447d88d99a7c4988d4aa394879a6f4546de2d38
Bilalkhan123456789 <bk315818@gmail.com>
2023-07-06 13:43:04 +0500

    first

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import  Navigation  from "./Config/Navigation.jsx";
+import Header from './Components/Header.jsx'
+
+
+function App() {
+  return (
+   <>
+   <Header/>
+   <Navigation/>
+   </>
+  );
+}
+
+export default App;
---
+++ src/Config/Navigation.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+
+function Navigation() {
+  return (
+    <div>
+<Routes>
+    <Route path={"/"} ></Route>
+</Routes>
+
+
+      
+    </div>
+  )
+}
+
+export default Navigation
